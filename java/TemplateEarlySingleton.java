d1158db1ac9662e556a8e7cb9b0c332d0353cf7b
neoNovadry <36753804+keyboard2769@users.noreply.github.com>
2019-08-12 17:09:43 +0800

    Create TemplateEarlySingleton.java

---
+++ java/TemplateEarlySingleton.java
@@ -0,0 +1,11 @@
+
+public final class ${name} {
+
+  private static final ${name} SELF = new ${name}();
+  public static final ${name} ccGetInstance(){return SELF;}//+++
+  private ${name}(){}//++!
+
+  //===
+
+ }//***eof
+ 
